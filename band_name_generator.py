5dccae52d799fe72a56cf9425653be360953e47e
Bryan Flanagan <bryan.d.flanagan@gmail.com>
2023-10-24 12:16:23 -0600

    feat: band name generator

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to your Band Name Generator.")
+street = input("What street do you live on?\n")
+car = input("What brand of car do you drive?\n")
+print("Your band name is " + street + " " + car )
