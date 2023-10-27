f9b1291853f12ec6dcb3328daf8fa57e580b82f2
Bryan Flanagan <bryan.d.flanagan@gmail.com>
2023-10-27 07:25:25 -0600

    Feat: finish string manipulation lesson

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,2 +1,5 @@
 print("Hello World\nHello World\nHello World")
 print("Hello" + " " + "World" + "!")
+#no indentations or spaces at beginning of code lines, will throw error
+#line below will throw ' IndentationError: unexpected indent '
+#   print("Hello World")
