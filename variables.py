0c8b3552ca4fda47e4b32658017bbe5d8106eeff
Bryan Flanagan <bryan.d.flanagan@gmail.com>
2023-10-27 07:36:39 -0600

    First exercise

---
+++ variables.py
@@ -0,0 +1,9 @@
+name = input("What is your name?\n")
+print("Hello" + " " + name)
+
+name = "Jeff"
+print("Hello" + " " + name)
+
+name = input ("What is your name?")
+length = len(name)
+print(length)
