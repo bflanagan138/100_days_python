bbbef6d195b1aaa8f945afb035124af41c8ab873
Bryan Flanagan <bryan.d.flanagan@gmail.com>
2023-10-27 07:43:07 -0600

    Second exercise

--- variables.py
+++ variables.py
@@ -1,9 +1,19 @@
-name = input("What is your name?\n")
-print("Hello" + " " + name)
+# name = input("What is your name?\n")
+# print("Hello" + " " + name)
 
-name = "Jeff"
-print("Hello" + " " + name)
+# name = "Jeff"
+# print("Hello" + " " + name)
 
-name = input ("What is your name?")
-length = len(name)
-print(length)
+# name = input ("What is your name?")
+# length = len(name)
+# print(length)
+
+a = input()
+b = input()
+
+c = a
+a = b
+b = c 
+
+print("a: " + a)
+print("b: " + b)
