24eb7efd71e98ba66d000ef4c7f843467a5dc8d8
Bryan Flanagan <bryan.d.flanagan@gmail.com>
2023-10-30 15:00:57 -0600

    feat: complete lesson

--- type_check_and_conversion.py
+++ type_check_and_conversion.py
@@ -1,3 +1,8 @@
 name = len(input("What is your name?"))
 num_char_str = str(name)
 print("Your name has " + num_char_str + " characters")
+
+float_a = float(12345)
+print(float_a)
+
+print(str(float_a + 123.56))
