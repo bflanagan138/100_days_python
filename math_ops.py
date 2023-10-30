75a2654d92d8a64c6e0b8e0a697f84e7128d44f8
Bryan Flanagan <bryan.d.flanagan@gmail.com>
2023-10-30 15:43:54 -0600

    feat: write bmi calculator

--- math_ops.py
+++ math_ops.py
@@ -1,14 +1,28 @@
-add = 3 + 5 #three plus five
-subtract = 3 - 5 #three minus five
-#addition, subtraction, multiplication will return integers unless specified
-#division will return floats unless specified
-divide = 3 / 5 #three divided by five
-multiply = 3 * 5 #three times five
-exponent = 3 ** 5 #three to the fifth power
+# add = 3 + 5 #three plus five
+# subtract = 3 - 5 #three minus five
+# #addition, subtraction, multiplication will return integers unless specified
+# #division will return floats unless specified
+# divide = 3 / 5 #three divided by five
+# multiply = 3 * 5 #three times five
+# exponent = 3 ** 5 #three to the fifth power
 
-print(add)
-print(subtract)
-print(divide)
-print(multiply)
-print(exponent)
+# print(add)
+# print(subtract)
+# print(divide)
+# print(multiply)
+# print(exponent)
+
+#bmi index calculator
+#bmi measures weight in kilograms divided by height in meters
+
+# simple bmi calculator 
+#input weight in kg
+weight = input("Please enter your weight in kg ")
+#input height in meters
+height = input("Please enter your height in meters ")
+#calculate bmi
+bmi = float(weight) / float(height) ** 2
+#print output of bmi
+print("Your BMI is " + str(bmi))
+
 
