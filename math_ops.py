977d202a2d1defb598b7e406af905abeae17ee76
Bryan Flanagan <bryan.d.flanagan@gmail.com>
2023-10-30 15:23:05 -0600

    feat: complete math ops lesson

---
+++ math_ops.py
@@ -0,0 +1,14 @@
+add = 3 + 5 #three plus five
+subtract = 3 - 5 #three minus five
+#addition, subtraction, multiplication will return integers unless specified
+#division will return floats unless specified
+divide = 3 / 5 #three divided by five
+multiply = 3 * 5 #three times five
+exponent = 3 ** 5 #three to the fifth power
+
+print(add)
+print(subtract)
+print(divide)
+print(multiply)
+print(exponent)
+
