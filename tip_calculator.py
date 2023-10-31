02796ed3f3024a35d01ad9b38003de5ca19bce8d
Bryan Flanagan <bryan.d.flanagan@gmail.com>
2023-10-30 20:08:34 -0600

    refactor: update output money format

--- tip_calculator.py
+++ tip_calculator.py
@@ -4,4 +4,4 @@ tip = input("What percentage tip would you like to give? 10, 12, or 15? ")
 tip_percentage = float(tip) / 100
 people = input("How many people to split the bill? ")
 per_person = round(((float(total) * float(tip_percentage)) + float(total)) / float(people), 2)
-print(f"Each person should pay: ${per_person} ")
+print(f"Each person should pay: ${per_person:.2f} ")
